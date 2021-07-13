1084a69eb6a47dfd2378bfc5450032f4256d4b30
Andres Reyes 👋 <ar97yf@gmail.com>
2021-07-13 11:01:06 -0500

    [ADD] configuration storybook

---
+++ .storybook/preview.js
@@ -0,0 +1,11 @@
+import '../styles/main.css'
+import '../styles/tokens.css'
+export const parameters = {
+  actions: { argTypesRegex: '^on[A-Z].*' },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+}

151787d3fc8201036e480fa97d8cdf72976e324e
Andres Reyes 👋 <ar97yf@gmail.com>
2021-07-16 16:09:09 -0500

    [ADD] snapshot stories with jest

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,12 +1,12 @@
 import 'minireset.css'
 import '../styles/globals.css'
 import '../styles/tokens.css'
-export const parameters = {
-  actions: { argTypesRegex: '^on[A-Z].*' },
-  controls: {
-    matchers: {
-      color: /(background|color)$/i,
-      date: /Date$/,
-    },
-  },
-}
+// export const parameters = {
+//   actions: { argTypesRegex: '^on[A-Z].*' },
+//   controls: {
+//     matchers: {
+//       color: /(background|color)$/i,
+//       date: /Date$/,
+//     },
+//   },
+// }
--- molecules/ButtonIcon/ButtonIcon.js
+++ molecules/ButtonIcon/ButtonIcon.js
@@ -1,23 +1,22 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
+import Spacer from '../../layout/Spacer'
 import Button from '../../atoms/Button'
 import Icon from '../../atoms/Icon'
 
-function ButtonIcon({ children, type, icon }) {
-  return (
-    <Button type={type} isBlock={false}>
-      <span style={{ marginRight: 10 }}>{children}</span>{' '}
-      {/* FIXME: Add vertical spacer copmponent */}
-      <Icon type={icon} />
-    </Button>
-  )
-}
+const ButtonIcon = ({ children, type, icon }) => (
+  <Button type={type} isBlock={false}>
+    {children}
+    <Spacer.Vertical size="xs"></Spacer.Vertical>
+    <Icon type={icon} />
+  </Button>
+)
 
 ButtonIcon.propTypes = {
   children: PropTypes.node.isRequired,
-  type: PropTypes.oneOf[('primary', 'secondary', 'tertiary')],
   icon: PropTypes.string.isRequired,
+  type: PropTypes.oneOf(['primary', 'secondary', 'tertiary']),
 }
 
 ButtonIcon.defaultProps = {

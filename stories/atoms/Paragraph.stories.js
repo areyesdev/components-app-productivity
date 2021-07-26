6cb796a31db9133423303559b55171e1b09ce4ba
Andres Reyes 👋 <areyesdev@gmail.com>
2021-07-26 18:42:41 -0500

    [UPDATE] Script create-component

---
+++ stories/atoms/Paragraph.stories.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import Paragraph from '../../atomic/Paragraph'
+
+export default {
+  title: 'Atomic/Paragraph',
+  component: Paragraph,
+}
+
+export const Default = () => <Paragraph>Hello Paragraph</Paragraph>

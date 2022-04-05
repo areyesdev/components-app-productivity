0bc6195d769e80260c6c67a0bb4c2114324cf01c
Andres Reyes <areyesdev@gmail.com>
2022-04-05 11:11:46 -0500

    feat(atoms, molecules): add new features molecules

--- atoms/Modal/Modal.js
+++ atoms/Modal/Modal.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 
 import styles from './Modal.module.css'
@@ -6,27 +6,74 @@ import { options } from './constants'
 import withStyles from '../../hocs/withStyles'
 import Icon from '../Icon'
 import Container from '../../layout/Container'
+import isEmpty from '../../utils/isEmpty'
 
-const handleClose = ({ onClose }) => () => {
-  onClose()
+import useMedia from '../../hook/useMedia'
+
+// Sync with ./Modal.module.css#L13-L17
+const FADE_OUT_ANIMATION_TIME = 400
+
+const createHandlerClick = () => (event) => {
+  event?.stopPropagation()
 }
 
-export const Modal = ({ onClose, children, getStyles, isPlayground }) => {
+const createHandlerClose = ({ onClose, setOnFadeOut }) => () => {
+  setOnFadeOut(true)
+  setTimeout(onClose, FADE_OUT_ANIMATION_TIME)
+}
+
+const handleSecondaryAction = ({ onSecondaryAction }) => () => {
+  onSecondaryAction()
+}
+
+export const Modal = ({
+  onClose,
+  secondaryAction,
+  children,
+  getStyles,
+  type,
+  isPlayground,
+}) => {
+  const isDesktop = useMedia(['(min-width: 992px)'], [true])
+  const [onFadeOut, setOnFadeOut] = useState(false)
+
+  const handleClose = createHandlerClose({ onClose, setOnFadeOut })
+
   return (
     <div
-      className={getStyles('modal', ['type'], {
+      className={getStyles('backdrop', {
         'is-playground': isPlayground,
+        'on-fade-out': onFadeOut,
       })}
+      onClick={handleClose}
     >
-      {!!onClose && (
-        <Icon
-          color="inverted"
-          name="angleLeft"
-          background="muted"
-          onClick={handleClose({ onClose })}
-        />
-      )}
-      <Container>{children}</Container>
+      <div
+        className={getStyles('modal', ['type'])}
+        onClick={createHandlerClick()}
+      >
+        <div className={getStyles('heading')}>
+          {!!onClose && (
+            <Icon
+              color={isDesktop && type === 'secondary' ? 'primary' : 'inverted'}
+              name={isDesktop ? 'cross' : 'angleLeft'}
+              background={isDesktop ? 'transparent' : 'muted'}
+              onClick={handleClose}
+              size={isDesktop ? 'lg' : 'md'}
+            />
+          )}
+          {secondaryAction && !isEmpty(secondaryAction) && (
+            <Icon
+              color="inverted"
+              name={secondaryAction?.icon}
+              background="muted"
+              onClick={handleSecondaryAction({
+                onSecondaryAction: secondaryAction?.handler,
+              })}
+            />
+          )}
+        </div>
+        <Container>{children}</Container>
+      </div>
     </div>
   )
 }
@@ -35,6 +82,10 @@ Modal.propTypes = {
   children: PropTypes.node.isRequired,
   getStyles: PropTypes.func.isRequired,
   onClose: PropTypes.func,
+  secondaryAction: PropTypes.shape({
+    icon: PropTypes.string,
+    handler: PropTypes.func,
+  }),
   type: PropTypes.oneOf(options.types),
   isPlayground: PropTypes.bool,
 }
